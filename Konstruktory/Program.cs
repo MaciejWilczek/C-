48a3f5df0ec1e8b4187a88c945b6b2c220edc8e3
Maciej Wilczek <ut_4247@zsp3zamosc.pl>
2025-10-09 10:23:36 +0200

    Dodanie pralki i konstruktorów

---
+++ Konstruktory/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Konstruktory
+{
+    class Tablica
+    {
+        public int[] tablica = new int[3];
+
+        public Tablica(int[] tablica)
+        {
+            this.tablica = tablica;
+        }
+        public Tablica()
+        {
+            tablica = [0,0,0];
+        }
+        public Tablica(Tablica tab)
+        {
+            //this.tablica = tab.tablica;
+            for (int i = 0; i < tab.tablica.Length; i++)
+            {
+                tablica[i] = tab.tablica[i];
+            }
+        }
+        public override string ToString()
+        {
+            string s = "";
+            foreach (int i in tablica) 
+                s += i.ToString();
+            return s;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tablica t1 = new Tablica([1,2,3]);
+            Console.WriteLine(t1);
+            Tablica t2 = new Tablica();
+            Console.WriteLine(t2);
+            Tablica t3 = new Tablica(t1);
+            Console.WriteLine(t3);
+        }
+    }
+}
---
+++ Pralka/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Pralka
+{
+    class Pralka {
+        private int program = 1;
+        private int wirowanie = 0;
+
+        
+        public void Start()
+        {
+
+        }
+        public void UstawProgram(string akcja)
+        {
+            if (akcja == "p+")
+            {
+                this.program += 1;
+                if (this.program >= 6)
+                    this.program = 1;
+            }
+            else
+            {
+                this.program -= 1;
+                if (this.program <= 0)
+                    this.program = 5;
+            }
+            Console.WriteLine("Ustawiono program nr: " + this.program);
+        }
+        public void UstawWirowanie(string akcja)
+        {
+
+        }
+        private void Pranie()
+        {
+
+        }
+        private void Plukanie()
+        {
+
+        }
+        private void Wirowanie()
+        {
+            
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pralka p = new Pralka();
+            p.UstawProgram("p+");
+        }
+    }
+}
