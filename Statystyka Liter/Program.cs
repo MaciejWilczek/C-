163283087630c9e33da186f6758b5e129f0f9f7c
Maciej Wilczek <ut_4247@zsp3zamosc.pl>
2025-11-20 10:23:50 +0100

    zadanie kolejne

---
+++ Statystyka Liter/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Statystyka_liter
+{
+    internal class Program
+    {
+        private static string StatystykaZnaków(string s)
+        {
+            SortedDictionary<char, int> Litery = new SortedDictionary<char, int>();
+            string wynik = string.Empty;
+
+            foreach (char c in s)
+                if (Litery.ContainsKey(c))
+                    Litery[c] += 1;
+                else
+                    Litery.Add(c, 1);
+            foreach (var e in Litery)
+                wynik += $"{e.Key}: {e.Value} \n";
+            return wynik;
+
+        }   
+        static void Main(string[] args)
+        {
+            string s = string.Empty;
+            Random r = new Random();
+            for (int i = 0; i < 1000; i++)
+                s += (char)r.Next('a', 'z' + 1);
+            Console.WriteLine(s);
+            Console.WriteLine(StatystykaZnaków(s));
+
+        }
+    }
+}
